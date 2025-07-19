47c621982e8e52a9b08d60561069b54e1457bcc2
Abdul Salam Khan <james7444555666@gmail.com>
2025-07-20 00:49:36 +0530

    Update set1.sql

--- set1.sql
+++ set1.sql
@@ -5,21 +5,21 @@ from employee
 order by levels
 limit 1
 
--/Q2: Which countries have the most Invoices?/-
+--Q2: Which countries have the most Invoices?--
 
 select invoice_id,billing_city,total
 from invoice
 order by total desc
 limit 1
 
--/Q3: What are top 3 values of total invoice?/-
+--Q3: What are top 3 values of total invoice?--
 
 select total
 from invoice
 order by total desc
 limit 3
 
--/Q4: Which city has the best customers? We would like to throw a promotional Music Festival in the city we made the most money. 
+--Q4: Which city has the best customers? We would like to throw a promotional Music Festival in the city we made the most money. --
 Write a query that returns one city that has the highest sum of invoice totals. 
 Return both the city name & sum of all invoice totals/-
 
@@ -29,7 +29,7 @@ from invoice
 group by billing_city
 order by invoicetotal desc
 
--/Q5: Who is the best customer? The customer who has spent the most money will be declared the best customer. 
+--Q5: Who is the best customer? The customer who has spent the most money will be declared the best customer.-- 
 Write a query that returns the person who has spent the most money./-
 
 select customer_id,sum (total)as totalspending
